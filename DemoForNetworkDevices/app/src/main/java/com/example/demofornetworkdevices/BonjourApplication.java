65a889e53addadda9f41947aad09a964f2622c29
Nabin <69750066+nabin0@users.noreply.github.com>
2023-09-19 18:31:47 +0530

    Update BonjourApplication.java

--- DemoForNetworkDevices/app/src/main/java/com/example/demofornetworkdevices/BonjourApplication.java
+++ DemoForNetworkDevices/app/src/main/java/com/example/demofornetworkdevices/BonjourApplication.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2015 Andriy Druk
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.demofornetworkdevices;
 
 import android.app.Application;
@@ -42,7 +27,7 @@ public class BonjourApplication extends Application {
             StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                     .detectDiskReads()
                     .detectDiskWrites()
-                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .detectNetwork()   
                     .penaltyLog()
                     .build());
             StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
@@ -68,7 +53,6 @@ public class BonjourApplication extends Application {
     }
 
     private Rx2Dnssd createDnssd() {
-        // https://developer.android.com/about/versions/12/behavior-changes-12#mdnsresponder
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             Log.i(TAG, "Using embedded version of dns sd");
             return new Rx2DnssdEmbedded(this);
